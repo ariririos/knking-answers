5a3cd7e0382ebbf24b2451c56ba2a65c1956604c
Ari Rios <aririos0719@gmail.com>
2025-05-22 18:47:50 -0400

    Simplified 11.6

--- 11/11.6.c
+++ 11/11.6.c
@@ -7,16 +7,8 @@ int compare(const void *a, const void *b) {
 
 void find_two_largest(int a[], int n, int *largest, int *second_largest) {
     qsort(a, n, sizeof(int), compare);
-    int max = 0, next_max = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] > max) {
-            int t = max;
-            max = a[i];
-            next_max = t;
-        }
-    }
-    *largest = max;
-    *second_largest = next_max;
+    *largest = a[n];
+    *second_largest = a[n-1]
 
 }
 
